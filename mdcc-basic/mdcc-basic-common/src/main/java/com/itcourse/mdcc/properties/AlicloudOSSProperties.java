4539cf96eec77f28050115368c4dcc356e70c655
daemon <lyk20040402@163.com>
2024-07-15 20:51:14 +0800

    文件上传功能开发

---
+++ mdcc-basic/mdcc-basic-common/src/main/java/com/itcourse/mdcc/properties/AlicloudOSSProperties.java
@@ -0,0 +1,16 @@
+package com.itcourse.mdcc.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "file.alicloud")
+public class AlicloudOSSProperties {
+
+    private String bucketName;
+    private String accessKey;
+    private String secretKey;
+    private String endpoint;
+}
