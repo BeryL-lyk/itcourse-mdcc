60294bb41e5c27a927d886c4216cb568975bdaed
daemon <lyk20040402@163.com>
2024-07-17 09:13:16 +0800

    媒体服务搭建

---
+++ mdcc-service/mdcc-service-media/src/main/java/com/itcourse/mdcc/config/MybatisPlusConfig.java
@@ -0,0 +1,22 @@
+package com.itcourse.mdcc.config;
+import com.baomidou.mybatisplus.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+//mapper接口扫描
+@MapperScan("com.itcourse.mdcc.mapper")
+//事务管理
+@EnableTransactionManagement
+public class MybatisPlusConfig {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
